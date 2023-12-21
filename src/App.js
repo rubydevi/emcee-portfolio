7d1cd03279f1d8a723cfa23d43e6500c97aa0744
iamrubydevi <ariesabyss@gmail.com>
2023-12-21 17:17:05 +0530

    Import all the required pages in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Navbar from './pages/About';
+import Navbar from './pages/Portfolio';
+import Navbar from './pages/Contact';
 import './App.css';
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <About />
+      <Portfolio />
+      <Contact />
     </div>
   )
 }
