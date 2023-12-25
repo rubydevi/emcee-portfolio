776291ee54243c9d3f94b6baf7a3af5e9ecb0bb3
iamrubydevi <ariesabyss@gmail.com>
2023-12-25 22:16:31 +0530

    Change route for home

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => (
     <Navbar />
     <Routes>
       <Route path="/" element={<About />} />
+      <Route path="/about" element={<About />} />
       <Route path="/portfolio" element={<Portfolio />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
