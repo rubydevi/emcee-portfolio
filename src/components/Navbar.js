a9918eced4786aa7a6e979a9c8ad59d4ca0ba12b
rubydevi <ariesabyss@gmail.com>
2024-01-04 20:06:11 +0530

    Update the navbar use the latest style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav>
-    <h1>
-      <Link to="/">My Portfolio</Link>
-    </h1>
-    <ul>
-      <li>
-        <Link to="/about">About Me</Link>
-      </li>
-      <li>
-        <Link to="/portfolio">Portfolio</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-    </ul>
-  </nav>
+  <header id="header" className="fixed-top">
+    <div className="container-fluid d-flex justify-content-between align-items-center">
+      <h1 className="logo me-auto me-lg-0">
+        <Link to="/">Anchor Amrita</Link>
+      </h1>
+      <nav id="navbar" className="navbar order-last order-lg-0">
+        <ul>
+          <li>
+            <NavLink to="/about">About Me</NavLink>
+          </li>
+          <li>
+            <NavLink to="/portfolio">Portfolio</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className="header-social-links">
+        <Link to="/contact" className="twitter"><i className="bi bi-twitter" /></Link>
+        <Link to="/contact" className="facebook"><i className="bi bi-facebook" /></Link>
+        <Link to="/contact" className="instagram"><i className="bi bi-instagram" /></Link>
+        <Link to="/contact" className="linkedin"><i className="bi bi-linkedin" /></Link>
+      </div>
+    </div>
+  </header>
 );
 
 export default Navbar;
