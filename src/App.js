6489176a4598570ca55161481111c44937542299
rubydevi <ariesabyss@gmail.com>
2024-01-06 12:10:44 +0530

    Import the css files on App instead

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import Header from './pages/Header';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
+import './assets/vendor/aos/aos.css';
+import './assets/vendor/bootstrap/css/bootstrap.min.css';
+import './assets/vendor/bootstrap-icons/bootstrap-icons.css';
+import './assets/vendor/boxicons/css/boxicons.min.css';
+import './assets/vendor/glightbox/css/glightbox.min.css';
+import './assets/vendor/swiper/swiper-bundle.min.css';
 import './assets/css/style.css';
 
 const App = () => (
