161db9d2eb3450044008370081a510837d423074
iamrubydevi <ariesabyss@gmail.com>
2024-01-10 21:43:04 +0530

    Add Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Footer = () => (
-  <div>Footer</div>
+  <footer id="footer">
+    <div className="container">
+      <div className="copyright">
+        &copy; Copyright
+        {' '}
+        <strong><span>Amrita</span></strong>
+        . All Rights Reserved
+      </div>
+      <div className="credits">
+        Designed by
+        {' '}
+        <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer>
 );
 
 export default Footer;
